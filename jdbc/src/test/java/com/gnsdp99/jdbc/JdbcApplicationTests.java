467ef5ca085c1e34fc01cd680a5148bc248cabed
gnsdp99 <rladpgns99@gmail.com>
2024-12-06 14:23:27 +0900

    init jdbc module

---
+++ jdbc/src/test/java/com/gnsdp99/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnsdp99.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
