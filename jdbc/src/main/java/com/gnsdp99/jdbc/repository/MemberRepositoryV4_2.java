766f55909f2f764d159b0cdcb8909c2bb7f79888
gnsdp99 <rladpgns99@gmail.com>
2024-12-09 22:05:59 +0900

    계좌 이체 - 스프링 예외 추상화 적용

---
+++ jdbc/src/main/java/com/gnsdp99/jdbc/repository/MemberRepositoryV4_2.java
@@ -0,0 +1,149 @@
+package com.gnsdp99.jdbc.repository;
+
+import com.gnsdp99.jdbc.domain.Member;
+import com.gnsdp99.jdbc.exception.MyDbException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * SQLExceptionTranslator 추가
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberRepositoryV4_2 implements MemberRepository {
+
+    private final DataSource dataSource;
+    private final SQLExceptionTranslator exceptionTranslator;
+
+    @Override
+    public Member save(Member member) {
+        String sql = """
+                insert into member(member_id, money)
+                values (?, ?)
+                """;
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            // 파라미터 바인딩
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            // 쓰기 작업 실행
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            throw exceptionTranslator.translate("save", sql, e);
+        } finally {
+            // SQL 작업에 실패해도 리소스를 반드시 해제해야 함.
+            close(conn, pstmt, null);
+        }
+    }
+
+    @Override
+    public Member findById(String memberId) {
+        String sql = """
+                select * from member
+                where member_id = ?
+                """;
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            // 읽기 작업 실행
+            if (rs.next()) {
+                // ResultSet에 데이터가 존재함
+                return new Member(
+                        rs.getString("member_id"),
+                        rs.getInt("money")
+                );
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            throw exceptionTranslator.translate("findById", sql, e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public void update(String memberId, int money) {
+        String sql = """
+                update member
+                set money = ?
+                where member_id = ?
+                """;
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw exceptionTranslator.translate("update", sql, e);
+        } finally {
+            close(conn, pstmt, null);
+        }
+    }
+
+    @Override
+    public void delete(String memberId) {
+        String sql = """
+                delete from member
+                where member_id = ?
+                """;
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw exceptionTranslator.translate("delete", sql, e);
+        } finally {
+            close(conn, pstmt, null);
+        }
+    }
+
+    private void close(Connection conn, Statement stmt, ResultSet rs) {
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        // 트랜잭션 동기화 - DataSourceUtils
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
+    private Connection getConnection() throws SQLException {
+        // 트랜잭션 동기화 - DataSourceUtils
+        Connection conn = DataSourceUtils.getConnection(dataSource);
+        logConeection(conn);
+        return conn;
+    }
+
+    private void logConeection(Connection conn) {
+        log.info("connection={}, class={}", conn, conn.getClass());
+    }
+}
