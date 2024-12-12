83cdfc30fc9f5905b8e210191a66d78f1ae11c67
gnsdp99 <rladpgns99@gmail.com>
2024-12-12 14:29:44 +0900

    itemservicedb - Querydsl 적용

---
+++ itemservice-db/src/main/java/com/gnsdp99/itemservicedb/repository/jpa/JpaItemRepositoryV3.java
@@ -0,0 +1,76 @@
+package com.gnsdp99.itemservicedb.repository.jpa;
+
+import com.gnsdp99.itemservicedb.domain.Item;
+import com.gnsdp99.itemservicedb.repository.ItemRepository;
+import com.gnsdp99.itemservicedb.repository.ItemSearchCondDto;
+import com.gnsdp99.itemservicedb.repository.ItemUpdateDto;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.gnsdp99.itemservicedb.domain.QItem.*;
+
+@Transactional
+@Repository
+public class JpaItemRepositoryV3 implements ItemRepository {
+
+    private final EntityManager entityManager;
+    private final JPAQueryFactory query;
+
+    public JpaItemRepositoryV3(EntityManager entityManager) {
+        this.entityManager = entityManager;
+        this.query = new JPAQueryFactory(entityManager);
+    }
+
+    @Override
+    public Item save(Item item) {
+        entityManager.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = entityManager.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = entityManager.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCondDto cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        return query
+                .select(item)
+                .from(item)
+                .where(likeItemName(itemName), loeMaxPrice(maxPrice))
+                .fetch();
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if (StringUtils.hasText(itemName)) {
+            return item.itemName.like("%" + itemName + "%");
+        }
+        return null;
+    }
+
+    private BooleanExpression loeMaxPrice(Integer maxPrice) {
+        if (maxPrice != null) {
+            return item.price.loe(maxPrice);
+        }
+        return null;
+    }
+}
