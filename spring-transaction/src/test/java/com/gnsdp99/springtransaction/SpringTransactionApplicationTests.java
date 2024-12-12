396ee790add9e0be4acaf31fdce84bce2ed8fa2f
gnsdp99 <rladpgns99@gmail.com>
2024-12-12 22:10:16 +0900

    init spring-transaction module

---
+++ spring-transaction/src/test/java/com/gnsdp99/springtransaction/SpringTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnsdp99.springtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
